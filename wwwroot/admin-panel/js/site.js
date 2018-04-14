b3277cf8a08c0462ebdcb535ca1d8898e1741922
Artur Sokolov <artur.sokolov@tochka.com>
2018-04-15 03:46:19 +0500

    new files (lib, nuget)

--- wwwroot/admin-panel/js/site.js
+++ wwwroot/admin-panel/js/site.js
@@ -1,1 +1,32 @@
-﻿// Write your JavaScript code.
+﻿function StickyFooter() {
+    let heightBrowser = $(window).height();
+    let heightContainerBlock = $(document).height();
+
+    (heightContainerBlock <= heightBrowser) ? $("footer").addClass("fixed-bottom") : $("footer").removeClass("fixed-bottom");
+}
+
+$(document).ready(function() {
+    // Sticky Footer
+    StickyFooter();
+    $(window).resize(function () {
+        StickyFooter();
+    });
+
+    // Data Confirm
+    $("[data-confirm]").click(function() {
+        let message = $(this).attr("data-confirm");
+        if (!message) {
+            message = "Подтвердите свои действия.";
+        }
+
+        if (!confirm(message)) {
+            return false;
+        }
+    });
+
+    // MDBootstrap
+    $(".button-collapse").sideNav();
+
+    $('.mdb-select').material_select();
+    // END MDBootstrap
+});
